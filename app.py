261f20ffaba73214bc6a79cdb6e4d2e2f0048899
fl0rma <florma67@gmail.com>
2023-09-13 19:42:36 +0200

    added waitress for deploy env

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from PIL import Image
 import base64
-import io
 import cv2
 import numpy as np
 
@@ -50,6 +49,17 @@ def index():
 #This line is executed when a GET request is made to the root URL or after processing a POST request. It renders the "index.html" template without any additional context.
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 #This block checks if the script is being run directly (not imported as a module). 
 #If it's being run directly, the app.run() function starts the Flask development server. The debug=True parameter enables debugging mode.
+
+
+
+# commands for storing code in github
+
+# git init => only once. For initializing git version control in project
+# git add . => to add changes to the staging area
+# git commit -m "some message" => to create a new version with recent staged changes
+# git push origin master => to upload version and changes to github
